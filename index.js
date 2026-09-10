d0f08096bb4052a30ff6b5a979bb9c8a94da4a7e
AbdulRahmanStephie <stephensymon3@gmail.com>
2026-09-10 09:18:39 +0200

    cheks

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 //window.alert("Welcome to My Portfolio!");
 
 document.getElementById("Welcome").textContent = "Get to Know AbdulRahman Stephen";
-
-const aboutButton = document.getElementById("aboutButton");
-const aboutText = document.getElementById("KnowMe");
-
-aboutButton.addEventListener("click", () => {
-    aboutText.textContent = "Hi, I'm AbdulRahman Stephen. I enjoy learning, building projects, and growing my skills.";
-    aboutText.hidden = false;
-    aboutButton.textContent = "About Me";
-});
